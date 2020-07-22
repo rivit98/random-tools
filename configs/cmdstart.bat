77414edbd1325a4b102bbc47d59c73add0921236
Rivit <albert98g@gmail.com>
2020-07-22 15:12:04 +0200

    configs

---
+++ configs/cmdstart.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: chcp 65001
+:: set PYTHONIOENCODING=utf-8
+
+set path=%path%;C:\bin
+
+:: vbox
+set path=%path%;C:\Program Files\Oracle\VirtualBox
+
+:: minGW
+set path=%path%;C:\MinGW\bin
+
+:: javac
+set path=%path%;C:\Program Files\Java\jdk-13\bin
+
+:: qt
+set path=%path%;C:\Qt\5.11.1\mingw53_32\bin
+
+::7zip
+set path=%path%;C:\portable\7-Zip
+
+::gource
+set path=%path%;C:\portable\gource-0.51.win64
+
+color a
+cls
+:: echo READY!
---
+++ configs/youtube-dl-fb.bat
@@ -0,0 +1,4 @@
+:: to tylko do pobierania z fb
+
+@echo off
+youtube-dl -f bestvideo+bestaudio/best %1
